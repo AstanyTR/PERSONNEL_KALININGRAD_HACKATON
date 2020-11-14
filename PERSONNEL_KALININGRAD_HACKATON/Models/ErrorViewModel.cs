b88b4c34caf2f06dea5ce070743f100ab8168cf5
Pavel Grachev <44@c1.ru>
2020-11-14 23:55:58 +0300

    First commit

---
+++ PERSONNEL_KALININGRAD_HACKATON/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PERSONNEL_KALININGRAD_HACKATON.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
